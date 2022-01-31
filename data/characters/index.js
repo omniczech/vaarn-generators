8a752e325631cc12e68a914540b7fec8df9316cd
Michael Petschek <mike.petschek@formlabs.com>
2022-01-31 13:09:32 -0500

    Add other ancestries

--- data/characters/index.js
+++ data/characters/index.js
@@ -1,7 +1,23 @@
-import cacogen from "./cacogen.json"
-import truekin from "./true-kin.json"
-import synth from "./synths.json"
+import cacklemaw from "./cacklemaw.json";
+import cacogen from "./cacogen.json";
+import faa from "./faaNomad.json";
+import lithling from "./lithling.json";
+import mycomorph from "./mycomorph.json";
+import newbeast from "./newbeast.json";
+import planeyfolk from "./planeyfolk.json";
+import synth from "./synths.json";
+import truekin from "./true-kin.json";
 
-const allAncestries = [cacogen,synth,truekin]
+const allAncestries = [
+  cacklemaw,
+  cacogen,
+  faa,
+  lithling,
+  mycomorph,
+  planeyfolk,
+  newbeast,
+  synth,
+  truekin,
+];
 
-export default allAncestries
+export default allAncestries;
