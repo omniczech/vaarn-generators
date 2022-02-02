5b555bc4b89079b83e818c65cc25f4229e8577ee
Michael Petschek <mikepetschek@gmail.com>
2022-02-01 22:27:24 -0500

    Adds Rough of first full detail location

--- components/Location.js
+++ components/Location.js
@@ -1,5 +1,41 @@
-const Location = () => {
-  return <p>Location</p>;
+import { useState, useEffect } from "react";
+import regions from "../data/locations/regions";
+import { randomFromArray, rollAllTables, camelize } from "../data/utils";
+
+const Location = ({ number, refresh }) => {
+	const [locationType, setLocationType] = useState("");
+	const [details, setDetails] = useState([]);
+	useEffect(() => {
+		roll();
+	}, []);
+	useEffect(() => {
+		roll();
+	}, [refresh]);
+	useEffect(() => {
+		if (locationType && regions[camelize(locationType)]) {
+			setDetails(rollAllTables(regions[camelize(locationType)]));
+		}
+	}, [locationType, refresh]);
+	const roll = () => {
+		setLocationType(randomFromArray(regions.locationType));
+	};
+
+	return (
+		<>
+			<h3>
+				{number} {locationType}
+			</h3>
+			{details && (
+				<p>
+					{details.map((answer) => (
+						<>
+							{answer.rollName} {answer.value}
+						</>
+					))}
+				</p>
+			)}
+		</>
+	);
 };
 
 export default Location;
--- pages/region.js
+++ pages/region.js
@@ -1,34 +1,64 @@
 import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Location from "../components/Location";
+import { randomFromArray } from "../data/utils";
+import regions from "../data/locations/regions";
 const Region = () => {
-  const [locationCount, setLocationCount] = useState([1, 2, 3, 4]);
-  const changeCount = (e) => {
-    const i = 1;
-    setLocationCount(Array.from({ length: e.target.value }));
-  };
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Region</h1>
-        <label>
-          Number of locations{" "}
-          <select onChange={changeCount}>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-          </select>
-        </label>
-        <p>Choose a dice size for each location (Larger is has more variety)</p>
-        {locationCount.map((location) => {
-          return <Location key={location} />;
-        })}
-      </main>
-    </div>
-  );
+	const [locationCount, setLocationCount] = useState(["", "", "", ""]);
+	const [landscape, setLandscape] = useState("");
+	const [namesake, setNamesake] = useState("");
+	const [refresh, setRefresh] = useState(0);
+	const [storySeed, setStorySeed] = useState("");
+	const changeCount = (e) => {
+		setLocationCount(Array.from({ length: e.target.value }));
+	};
+	useEffect(() => {
+		roll();
+	}, []);
+	const roll = () => {
+		setLandscape(randomFromArray(regions.landscape));
+		setNamesake(randomFromArray(regions.namesake));
+		setStorySeed(
+			randomFromArray(regions.genericStorySeeds.npcA) +
+				" wants to " +
+				randomFromArray(regions.genericStorySeeds.wants) +
+				" " +
+				randomFromArray(regions.genericStorySeeds.npcB) +
+				". However, there is a complication in the form of " +
+				randomFromArray(regions.genericStorySeeds.complication)
+		);
+		setRefresh(refresh + 1);
+	};
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<h1>Region</h1>
+				<button onClick={roll}>Roll another one</button>
+				<p>
+					<b>Landscape:</b> {landscape}
+				</p>
+				<p>The region was named after {namesake}</p>
+				<p>
+					<b>Story Seed:</b> {storySeed}
+				</p>
+				<label>
+					Number of locations{" "}
+					<select onChange={changeCount}>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+					</select>
+				</label>
+				{/* <p>Choose a dice size for each location (Larger is has more variety)</p> */}
+				{locationCount.map((location, i) => {
+					return <Location key={i} number={i + 1} refresh={refresh} />;
+				})}
+			</main>
+		</div>
+	);
 };
 
 export default Region;
