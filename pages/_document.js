7654cff67c6159cf5ab1e25f1655b1cec2f7f12c
Michael Petschek <mikepetschek@gmail.com>
2022-02-01 19:27:47 -0500

    Fixes import for font

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&display=optional"
           rel="stylesheet"
         />
       </Head>
