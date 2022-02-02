78eb7ba32670a5e96724fc3a3493269a39be8ca8
Michael Petschek <mike.petschek@formlabs.com>
2022-02-02 16:58:53 -0500

    strip some chars in camelize function

--- data/utils.js
+++ data/utils.js
@@ -1,21 +1,24 @@
 const randomFromArray = (array) => {
-	return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 };
 
 const rollAllTables = (rolls) => {
-	if (!rolls) {
-		return null;
-	}
-	return rolls.map((roll) => {
-		return { rollName: roll.name, value: randomFromArray(roll.options) };
-	});
+  if (!rolls) {
+    return null;
+  }
+  return rolls.map((roll) => {
+    return { rollName: roll.name, value: randomFromArray(roll.options) };
+  });
 };
 
 const camelize = (str) => {
-	return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-		if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
-		return index === 0 ? match.toLowerCase() : match.toUpperCase();
-	});
+  return str
+    .replace("'", "")
+    .replace("-", "")
+    .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+      if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+      return index === 0 ? match.toLowerCase() : match.toUpperCase();
+    });
 };
 
 export { randomFromArray, rollAllTables, camelize };
