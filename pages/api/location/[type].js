3510f0f72090b5eb6c8c7e426cc99cc4b6e860a5
Michael Petschek <mike.petschek@formlabs.com>
2022-02-11 16:57:46 -0500

    Adds location by type api endpoint

---
+++ pages/api/location/[type].js
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { camelize, randomFromArray, rollAllTables } from "../../../data/utils";
+import regions from "../../../data/locations/regions";
+export default function handler(req, res) {
+  if (!req.query.type in regions) {
+    res.status(400).json({ error: "Bad request" });
+  } else {
+    const rolls = rollAllTables(regions[camelize(req.query.type)]);
+    const description = rolls.reduce((reducer, roll) => {
+      return reducer + roll.rollName + " " + roll.value;
+    }, "");
+    res.status(200).json({ type: req.query.type, description: description });
+  }
+
+  //   let i = 0;
+  //   const locations = [];
+  //   while (i < ) {
+  //     const type = randomFromArray(regions.locationType);
+  //     const location = {
+  //       type: type,
+  //       rolls: rollAllTables(regions[camelize(type)]),
+  //     };
+  //     locations.push(location);
+  //     i++;
+  //   }
+  //   res.status(200).json({ locations: locations });
+}
