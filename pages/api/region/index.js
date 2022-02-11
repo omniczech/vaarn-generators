8a91507162f39e498e9621a93a9a16babab90a0e
Michael Petschek <mike.petschek@formlabs.com>
2022-02-11 13:46:43 -0500

    adds single location generator

---
+++ pages/api/region/index.js
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { camelize, randomFromArray, rollAllTables } from "../../../data/utils";
+import regions from "../../../data/locations/regions";
+export default function handler(req, res) {
+  let i = 0;
+  const locations = [];
+  const type = randomFromArray(regions.locationType);
+  const location = {
+    type: type,
+    rolls: rollAllTables(regions[camelize(type)]),
+  };
+  locations.push(location);
+  i++;
+  res.status(200).json({ locations: locations });
+}
