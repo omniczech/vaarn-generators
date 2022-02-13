742acf4b489041266db7d36c0334d00852421f42
Michael Petschek <mike.petschek@formlabs.com>
2022-02-13 14:35:22 -0500

    Fix cors for region

--- pages/api/location/index.js
+++ pages/api/location/index.js
@@ -1,7 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
 import { camelize, randomFromArray, rollAllTables } from "../../../data/utils";
 import regions from "../../../data/locations/regions";
-export default function handler(req, res) {
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    origin: true,
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+export default async function handler(req, res) {
+  await cors(req, res);
   const type = randomFromArray(regions.locationType);
   const rolls = rollAllTables(regions[camelize(type)]);
   const description = rolls.reduce((reducer, roll) => {
--- pages/api/region/[num].js
+++ pages/api/region/[num].js
@@ -1,14 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
 import { camelize, randomFromArray, rollAllTables } from "../../../data/utils";
 import regions from "../../../data/locations/regions";
-export default function handler(req, res) {
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    origin: true,
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+export default async function handler(req, res) {
+  await cors(req, res);
   let i = 0;
   const locations = [];
   while (i < req.query.num) {
     const type = randomFromArray(regions.locationType);
+    const rolls = rollAllTables(regions[camelize(type)]);
     const location = {
       type: type,
-      rolls: rollAllTables(regions[camelize(type)]),
+      descriptio: rolls.reduce((reducer, roll) => {
+        return reducer + roll.rollName + " " + roll.value;
+      }, ""),
     };
     locations.push(location);
     i++;
