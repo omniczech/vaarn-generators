a77d0e125434990a374e87aebfe84c8a6a1a9451
Michael Petschek <mike.petschek@formlabs.com>
2022-02-13 14:15:46 -0500

    Fix CORS

--- pages/api/location/[type].js
+++ pages/api/location/[type].js
@@ -17,6 +17,7 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
+    origin: true,
     methods: ["GET", "POST", "OPTIONS"],
   })
 );
