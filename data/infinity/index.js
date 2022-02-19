727ec3bf67fb6b39a16fdb400d71fb27bae665c6
Michael Petschek <mike.petschek@formlabs.com>
2022-02-19 13:24:45 -0500

    Adds better styling, some detailed descriptions

--- data/infinity/index.js
+++ data/infinity/index.js
@@ -8,6 +8,11 @@ const infinityVault = {
     "Medical Facility",
     "Sleeping space",
     "Data Storage Room",
+    "Armory",
+    "Waste disposal",
+    "Water filtration and harvesting",
+    "An empty room",
+    "Large hallway",
   ],
   details: [
     "Flooded with sand",
@@ -55,6 +60,33 @@ const infinityVault = {
     "Colossal dead mollusk",
     "Hovering polychrome spheres",
     "Humming teleportation gate",
+    "An Exit - This room has a door to the outside world, players can exit from the vault if they so choose",
   ],
+  descriptions: {
+    Entrance:
+      "As you step down into the vault, you find yourself in an empty metal room, sand lines the edges of the room but the structure seems in good condition. From this room there are 4 barely lit hallways leading into other rooms in the vault.",
+    "Supply Storage":
+      "A derelict storage space. Boxes of maintenance supplies are everywhere, lights flickering and the smell of cleaning chemicals is everywhere.",
+    "Biology Lab":
+      "Containers with strangely shaped creatures in liquids are displayed prominently. Boards on the walls contain detailed scientific notes in a strange language.",
+    "Physics Lab": "",
+    Showers:
+      "Grimy and unkempt, the showers are long since abandoned. Molds grow on the tiles and the scent of mildew hits your nostrils the moment you enter.",
+    Kitchen:
+      "Left unattended far too long, the kitchen is all but unusable. This room reeks of rotting food and mold.",
+    "Medical Facility": "",
+    "Sleeping space":
+      "6 bunk beds, enough to sleep 12 humanoids are present, several lockers are present next to these sleeping positions.",
+    "Data Storage Room":
+      "Large computer banks line the walls of this room, a loud whirring of fans and other machinery is present throughout. The room smells sterile and dead",
+    Armory:
+      "Somewhat looted, this room has racks with various weaponry available on them",
+    "Waste disposal":
+      "Several disposal containers are here marked with symbols indicating the types of waste they should be used for. There's a faint stench in the room of rotting biological matter as well as sanitation supplies",
+    "Water filtration and harvesting": "",
+    "An empty room":
+      "Suspiciously clean and well kept, there is no obvious purpose or ",
+    "Large hallway": "",
+  },
 };
 export default infinityVault;
--- pages/the-infinity-vault.js
+++ pages/the-infinity-vault.js
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import Draggable from "react-draggable";
+import classNames from "classnames";
 import ColonRoll from "../components/colonRoll";
 import infinityVault from "../data/infinity";
 import { randomFromArray } from "../data/utils";
 import styles from "../styles/Infinity.module.css";
 
 const TheInfinityVault = () => {
-  const [locations, setlocations] = useState([
+  const exitString =
+    "An Exit - This room has a door to the outside world, players can exit from the vault if they so choose";
+  const initState = [
     [
       {
         name: "Entrance",
         description: "",
-        detail: randomFromArray(infinityVault.details),
-        detailB: randomFromArray(infinityVault.detailsB),
+        detail: "Flooded with sand",
+        detailB: exitString,
         visited: true,
       },
     ],
-  ]);
-
+  ];
+  const [locations, setlocations] = useState(initState);
   const [depth, setDepth] = useState(0);
   const [distance, setDistance] = useState(0);
   const [entrance, setEntrance] = useState([0, 0]);
@@ -47,7 +50,7 @@ const TheInfinityVault = () => {
       setlocations(temp);
     }
   }, [distance, depth, locations]);
-  const deeper = () => {
+  const south = () => {
     if (depth + 1 === locations.length) {
       setlocations(
         locations.concat([
@@ -59,7 +62,7 @@ const TheInfinityVault = () => {
     }
     setDepth(depth + 1);
   };
-  const backTheWayWeCame = () => {
+  const north = () => {
     if (depth >= 1) {
       setDepth(depth - 1);
     } else {
@@ -67,7 +70,6 @@ const TheInfinityVault = () => {
       const newLayer = locations[0].map((location) => {
         return {};
       });
-      console.log(newLayer, newLayer.concat(locations));
       setlocations([newLayer].concat(locations));
       setEntrance([entrance[0] + 1, entrance[1]]);
     }
@@ -94,6 +96,13 @@ const TheInfinityVault = () => {
       setDistance(distance + 1);
     }
   };
+
+  const resetVault = () => {
+    setDepth(0);
+    setDistance(0);
+    setEntrance([0, 0]);
+    setlocations(initState);
+  };
   const chooseLocation = (i, j) => {
     setDepth(i);
     setDistance(j);
@@ -111,62 +120,87 @@ const TheInfinityVault = () => {
     <main className={styles.main}>
       <h1>The Infinity Vault</h1>
       {/* <button>More info</button> */}
-      <div>
-        <div className={styles.flex}>
-          <div className={styles.map}>
-            {locations.map((layer, i) => {
-              return (
-                <div className={styles.layer} key={"layer" + i}>
-                  <p>Layer {i + 1}</p>
-                  {layer.length &&
-                    layer.map((location, j) => {
-                      const current = i === depth && j === distance;
-                      const isEntrance = i === entrance[0] && j === entrance[1];
-                      console.log(location.visited);
-                      return (
-                        <div
-                          className={
-                            isEntrance
-                              ? styles.entrance
-                              : current
-                              ? styles.current
-                              : location.visited
-                              ? styles.visited
-                              : styles.location
-                          }
-                          key={"room" + i + j}
-                          onClick={() => {
-                            chooseLocation(i, j);
-                          }}
-                        >
-                          {location.visited && (
-                            <p>
-                              {i},{j}
-                            </p>
-                          )}
-                          {/* {location.name} */}
-                        </div>
-                      );
-                    })}
-                </div>
-              );
-            })}
-          </div>
-          <div>
-            {currentLocation && (
-              <>
-                <h3>Details</h3>
-                <p>
-                  <b>{currentLocation.name}</b>
-                </p>
-                <ColonRoll rollName="Detail A" value={currentLocation.detail} />
-                <ColonRoll
-                  rollName="Detail B"
-                  value={currentLocation.detailB}
-                />
-              </>
-            )}
-          </div>
+      <div className={styles.flex}>
+        <div className={styles.map}>
+          {locations.map((layer, i) => {
+            return (
+              <div className={styles.layer} key={"layer" + i}>
+                <p>Layer {i + 1}</p>
+                {layer.length &&
+                  layer.map((location, j) => {
+                    const current = i === depth && j === distance;
+                    const isEntrance =
+                      (i === entrance[0] && j === entrance[1]) ||
+                      location.detailB === exitString;
+                    const hasUp = location.name && !i === 0;
+                    const hasDown =
+                      location.name &&
+                      !(i === locations.length - 1) &&
+                      locations[i + 1][j].name;
+                    const hasLeft =
+                      location.name && !(j === 0) && locations[i][j - 1].name;
+                    const hasRight =
+                      location.name &&
+                      !(j === locations[0].length - 1) &&
+                      locations[i][j + 1].name;
+                    console.log(i, locations.length, hasDown);
+                    return (
+                      <div
+                        className={classNames([`${styles.location}`], {
+                          [`${styles.entrance}`]: isEntrance,
+                          [`${styles.current}`]: current,
+                          [`${styles.visited}`]: location.visited,
+                        })}
+                        key={"room" + i + j}
+                        onClick={() => {
+                          chooseLocation(i, j);
+                        }}
+                      >
+                        {location.visited && (
+                          <p>
+                            {i},{j} {location.detailB === exitString && "¤"}
+                          </p>
+                        )}
+                        {/* {location.name} */}
+                        {hasUp && (
+                          <div
+                            className={classNames(styles.up, styles.hallway)}
+                          />
+                        )}
+                        {hasLeft && (
+                          <div
+                            className={classNames(styles.left, styles.hallway)}
+                          />
+                        )}
+                        {hasRight && (
+                          <div
+                            className={classNames(styles.right, styles.hallway)}
+                          />
+                        )}
+                        {hasDown && (
+                          <div
+                            className={classNames(styles.down, styles.hallway)}
+                          />
+                        )}
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {currentLocation && (
+            <>
+              <h3>Details</h3>
+              <p>
+                <b>{currentLocation.name}</b>
+              </p>
+              <p>{infinityVault.descriptions[currentLocation.name]}</p>
+              <ColonRoll rollName="Detail A" value={currentLocation.detail} />
+              <ColonRoll rollName="Detail B" value={currentLocation.detailB} />
+            </>
+          )}
         </div>
       </div>
       <Draggable>
@@ -175,7 +209,10 @@ const TheInfinityVault = () => {
             <b>Controls</b>
           </p>
           <p>
-            <button onClick={backTheWayWeCame}>⇑</button>
+            <button onClick={resetVault}>Reset</button>
+          </p>
+          <p>
+            <button onClick={north}>⇑</button>
             {/* <button>New path back the way we came</button> */}
           </p>
           <p>
@@ -183,7 +220,7 @@ const TheInfinityVault = () => {
             <button onClick={east}>⇒</button>
           </p>
           <p>
-            <button onClick={deeper}>⇓</button>
+            <button onClick={south}>⇓</button>
             {/* <button onClick={deeperNewPath}>Deeper new path</button> */}
           </p>
         </div>
