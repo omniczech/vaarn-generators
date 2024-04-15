7b0b588d338a1bdf9c8b3bf5b802b36d1a8be271
VedminStuden <140993177+VedminStuden@users.noreply.github.com>
2024-04-15 21:46:16 +0900

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <Link href="https://vaultsofvaarn.com/">Vaults of Vaarn</Link>는
         by <Link href="https://graculusdroog.itch.io/">Leo Hunt</Link>의 저작물이며, 이 생성기의 제작과 유지보수는{" "}
         <Link href="https://github.com/omniczech">Omniczech</Link>각 맡고 있습니다.{" "}  
-        이 한국어 페이지는 <Link href="https://github.com/omniczech">vedminstuden</Link>가 포크하여 수정한 것임을 밝힙니다.
+        이 한국어 페이지는 <Link href="https://github.com/VedminStuden">vedminstuden</Link>가 포크하여 수정한 것임을 밝힙니다.
       </p>
     </footer>
   );
