44ab1c3bf96044038a30e246852795f17e2b1e05
Michael Petschek <mikepetschek@gmail.com>
2024-04-22 13:45:39 -0400

    Add i18n wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
+import { appWithTranslation } from "next-i18next";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
