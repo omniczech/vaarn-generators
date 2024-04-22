901e495f013d83a1ea3708ad06508f443a73e3c6
Michael Petschek <mikepetschek@gmail.com>
2024-04-22 13:45:23 -0400

    Add internationalization

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ko"],
+  },
+  fallbackLng: {
+    default: ["en"],
+  },
+};
