abf79a3a53bba6fff04ba27b1d67d7486d9cf3fc
Michael Petschek <mikepetschek@gmail.com>
2024-04-27 21:55:21 -0400

    Add a fun header font

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@400;700&display=optional"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jersey+20&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />
