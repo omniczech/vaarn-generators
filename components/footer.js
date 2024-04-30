027740c82a82eaf61a1299bbe1f9b396d87aff29
Michael Petschek <mikepetschek@gmail.com>
2024-04-30 12:57:08 -0400

    Updating Translations

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
 import styles from "../styles/Home.module.css";
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <footer className={styles.footer}>
       <p>
-        <Link href="https://vaultsofvaarn.com/">Vaults of Vaarn</Link> is made
-        by <Link href="https://graculusdroog.itch.io/">Leo Hunt</Link>, these
-        generators are made and maintained by{" "}
+        <Link href="https://vaultsofvaarn.com/">{t("Vaults of Vaarn")}</Link>{" "}
+        {t("is made by")}{" "}
+        <Link href="https://graculusdroog.itch.io/">Leo Hunt</Link>,
+        {t("these generators are made and maintained by")}{" "}
         <Link href="https://github.com/omniczech">Omniczech</Link>
       </p>
     </footer>
